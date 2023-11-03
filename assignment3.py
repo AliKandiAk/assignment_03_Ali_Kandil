22aa3a30f9209246224ea30ae7c69a31b51a4f9b
AliKandiAk <141944549+AliKandiAk@users.noreply.github.com>
2023-11-03 21:42:03 +0200

    assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -74,6 +74,15 @@ def Calculate_Average(Data):#choice 7
     
 
 while True:
+  print("1. Get Student by ID")
+  print("2. Get All Students")
+  print("3. Get Students by Major")
+  print("4. Add Student")
+  print("5. Find Common Majors")
+  print("6. Delete Student")
+  print("7. Calculate Average GPA")
+  print("8. Get Top Performers")
+  print("9. Exit")
   x=input("Enter choice number:")
   if x=="1":
       id=int(input("enter student ID:"))
